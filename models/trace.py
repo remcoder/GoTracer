6eb4312a1ba59c4966db3d76092f9c830b7c0b03
Remco Veldkamp <remcoder@gmail.com>
2025-04-13 11:22:41 +0200

    remove deprecated application files and templates
    Updated to work locally w/o python

--- models/trace.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import db
-
-class Trace(db.Model):  
-  imageUrl = db.LinkProperty(required=True)
-  corners = db.ListProperty(item_type=int)
-  datetime = db.DateTimeProperty()
-  match = db.FloatProperty()
