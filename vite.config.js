f59ecfac4f61129c91dba82bb2a708994d4d11c6
Remco Veldkamp <remcoder@gmail.com>
2026-06-08 20:21:56 +0200

    Add linting and typescript

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./js/**/*.js"'
+      }
+    })
+  ],
   server: {
     port: 3000
   },
